67f03a15c3eb9064a3a9089ad6cd26d8614eba0b
ellomenop <ellomenop.speedruns@gmail.com>
2022-09-24 02:14:23 -0400

    Initial check in

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Noita Wiki Redirect
- 
+<img src="src/icons/icon_152.png"/>
+
+# Noita Wiki search
+
+This is a browser extension designed to be used in Google Chrome and chromium-based browsers (such as MS Edge) by players of the game Noita.
+The purpose of this extension is to provide two benefits:
+
+- An easy to use hotkeyed trigger (`ALT+N`) to enable users to search for information about the game using the [official community wiki](https://noita.wiki.gg/).
+- A redirect on searches and links from the old wiki to the new one
+
+This code base was inspired and repurposed from the Path of Exile wiki search extension which can be found [here](https://github.com/Project-Path-of-Exile-Wiki/poe-wiki-search).
---
+++ src/ui/ui.js
@@ -0,0 +1,48 @@
+// This is the URL we will use to append all your searches to.
+// If your search query directly matches a name of a wiki page - you will get taken
+// to this page directly. If not - you will be taken to a search results page.
+const defaultWikiURL = "https://noita.wiki.gg/index.php?search="
+
+// This just selects the search input box in the extension popup when the popup opens.
+// This way we reduce your action-to-search count to just `hotkey+search+ENTER`.
+const getSearchInput = () => document.querySelector("#searchWikiInput")
+
+// Function to focus on the search box.
+const focusSearch = (searchInput) => searchInput.focus()
+
+// Trivial validator saying - if a search query is empty after trimming any whitespace
+// at the start and end of your query - no point to even execute it.
+const validateQuery = (query) => query.trim() !== "" ? true : false
+
+/**
+ * Main handler method responsible for what happens when the extension popup fires.
+ *
+ * Any sub-handlers to go into this method.
+ */
+const handler = () => {
+    const searchInput = getSearchInput()
+    focusSearch(searchInput)
+
+    // While typing in the search input, we listen for a keyUP event for the key ENTER.
+    // If this is registered - we assume you pressed ENTER and proceed.
+    searchInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            // Get the value of your search input
+            const query = searchInput.value
+            // Check if empty or not
+            const valid = validateQuery(query)
+            if (valid) {
+                // If not empty - ask your browser to open a new tab/window (based on your browser setting)
+                // which takes you to the wiki search / page.
+                chrome.tabs.create({url: `${defaultWikiURL}${query}`})
+                // Close the popup.
+                window.close()
+            }
+
+        }
+    })
+}
+
+// Execute the handler method when the popup opens, so the event listener
+// gets registered for the ENTER key.
+handler()
